06e464c16f134069b1d9c6976732e57a9bfb92c1
pruthvill <84236916+pruthvill@users.noreply.github.com>
2024-06-16 01:50:16 +0530

    ok

--- app/components/Post.tsx
+++ app/components/Post.tsx
@@ -22,7 +22,7 @@ const Post: React.FC<PostProps> = ({ title, content }) => {
         </div>
 
         {/* Content */}
-        <ul className="flex flex-col w-full max-w-2xl mx-auto pl-6 pr-1">
+        <ul className="flex flex-col w-full max-w-2xl mx-auto pl-12 pr-4">
           <li>{content}</li>
         </ul>
       </div>
