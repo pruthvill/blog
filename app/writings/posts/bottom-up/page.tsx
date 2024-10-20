64a62e83d89c3762d0bd1a8136776991b7ee2d18
vil <84236916+pruthvill@users.noreply.github.com>
2024-10-21 01:29:37 +0530

    Update page.tsx

--- app/writings/posts/bottom-up/page.tsx
+++ app/writings/posts/bottom-up/page.tsx
@@ -1,35 +1,77 @@
-import Header from "@/app/components/Header";
-import Link from "next/link";
+import Post from "@/app/components/Post";
 
-const Media = () => {
-  return (
-    <div className="pruthvil  ">
-      <Header />
+const BottomUpPost = () => {
+  const postData = {
+    title: "Builiding life bottom up.",
+    content: (
+      <>
+        <p>
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Humans reproduce two ways, biologically using genes and culturally
+          using memes. In biological reproduction, we don't have a choice
+          regarding what genes we inherit. Our gender, race, and other traits passed on through genes are predetermined. 
+          Genes can only be transmitted from parent to offspring. Evolution occurs very slowly. In fact,
+          Humans have not undergone a major genetic change in the past 10,000
+          years and despite diverging from chimpanzees around 6 million years ago, we
+          still share 98.8% of our DNA with them.
+        </p>
 
-      <div className="max-w-xl mx-auto sm:px-6">
-        <h2 className="text-center text-lg font-bold mb-2 ">
-         On Advices
-        </h2>
+        <p>
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Memes, conversely, are disseminated through culture. Memes can
+          change drastically even within a single generation. As we mature, we
+          realize that we have the choice to accept or discard memes. As we accumulate
+          more life experience, we acquire additional sources of memes that
+          influence us directly or indirectly. Although we can choose which
+          memes to embrace or reject, most individuals subconsciously adopt the
+          memes passed down from society, thereby perpetuating its functioning.
+        </p>
 
-        <div className="flex flex-col mx-auto  ">
-          <ul className="flex flex-col gap-4 ">
-            <li>
-              <p className="mb-4 "> If you want to do better in any domain of life you have two ways to go about it.
-              First is to trying to figure out everything by yourself and advice from external sources. 
-              </p>
-              <p className="mb-4 "> If you try to figure out everhything by yourself it will take </p>
+        <p>
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;If you observe your day-to-day actions, you'll likely find that most, 
+          if not all, stem from the memes propagated by society. As Jung says,
+          "The world will ask who you are, and if you do not know, the world will tell you." 
+          When you lack intrinsic desires, society's desires often become yours.
+          The danger lies in adopting these external desires without question. 
+          If you fail to fulfill them, society may criticize you, and worse, you may berate yourself for neglecting your true aspirations. This cycle perpetuates a life driven by external validation rather than authentic self-realization.
+        </p>
 
-              
+        <p>
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Mimetic desire refers to the notion that individuals do not
+          autonomously desire objects, goals, or ambitions; instead, we learn
+          what to desire by observing the desires of others. We desire things
+          because we see others desiring them, not due to the inherent value of
+          the objects themselves.
+        </p>
 
-              <p className="mb-4 "> If you take someone elses advice to face value </p>
-              <p className="mb-4 "> </p>
-              <p className="mb-4 "> </p>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
-  );
+        <p>
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;The idea of something is often better than the actual thing itself,
+          and when you obtain it, the joy of acquisition will be much less than
+          expected and will fade quickly. Our wants reside in the future, yet
+          when we wish for them, we fail to comprehend the consequences of
+          attaining them. It's easy to forget that we are not the same as we were 5 years
+          ago; in fact, we are not even the same as a year ago. So how can you
+          yearn for something in the future and assume that our future self will
+          have the same desires?
+        </p>
+
+        <p>
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;People think they will work hard in their 20s, accumulate wealth,
+          retire in their 30s, then pursue their passions, relocate to a calm
+          place, and simply relax for the remainder of their lives. However,
+          when the time comes, even if they have amassed enough money, they may
+          realize they no longer want to do that, perhaps because their desires
+          have evolved, or they have acquired new responsibilities, or their values and priorities have shifted.
+        </p>
+
+        <p>
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Therefore, do what you want to do now because you may not want to do
+          it later. And remember, it's ok to have desires that others
+          may not comprehend.
+        </p>
+      </>
+    ),
+  };
+
+  return <Post {...postData} />;
 };
 
-export default Media;
+export default BottomUpPost;
