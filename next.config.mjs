9b8811ee4e82cce4ac0b3428dc253a531d98ade2
pruthvill <pruthvil@protonmail.com>
2025-05-16 11:34:32 +0530

    ok

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export", // Uncomment this line to enable static export
+  reactStrictMode: true,
+  swcMinify: true,
+};
 
 export default nextConfig;
+
